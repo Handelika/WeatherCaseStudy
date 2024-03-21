e04e885c317a0d3d38f901d8477465712a60aeb9
Handelika <46781563+Handelika@users.noreply.github.com>
2024-03-21 03:17:16 +0300

    data state deleted

--- app/src/main/java/com/handelika/weathercasestudy/data/DataState.kt
+++
@@ -1,7 +0,0 @@
-package com.handelika.weathercasestudy.data
-
-sealed class DataState<T> {
-    data class Success<T>(val data: T) : DataState<T>()
-    data class Loading<T>(val loading: Boolean) : DataState<T>()
-    data class Error<T>(val exception: Exception) : DataState<T>()
-}
